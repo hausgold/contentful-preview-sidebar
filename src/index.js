364db855bcd55f456a74c773d914936b2fdef2c7
Niklas Schreiber <niklas@derschreiber.com>
2022-04-07 17:50:24 +0200

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { Button } from '@contentful/forma-36-react-components';
+import { Button, Grid, Paragraph } from '@contentful/forma-36-react-components';
 import { init, locations } from 'contentful-ui-extensions-sdk';
-import tokens from '@contentful/forma-36-tokens';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
-
-export class DialogExtension extends React.Component {
-  static propTypes = {
-    sdk: PropTypes.object.isRequired
-  };
-
-  render() {
-    return (
-      <div style={{ margin: tokens.spacingM }}>
-        <Button
-          testId="close-dialog"
-          buttonType="muted"
-          onClick={() => {
-            this.props.sdk.close('data from modal dialog');
-          }}>
-          Close modal
-        </Button>
-      </div>
-    );
-  }
-}
 
 export class SidebarExtension extends React.Component {
   static propTypes = {
@@ -37,10 +15,43 @@ export class SidebarExtension extends React.Component {
     this.props.sdk.window.startAutoResizer();
   }
 
-  getUrl = async domain => {
-    const { entry, space, locales } = this.props.sdk;
-    const { slug: contentSlug, category, template } = entry.fields;
+  getCategorySlug = async (category, space, locales) => {
+    const categoryId = category.getValue().sys.id;
+    const sysCategory = await space.getEntry(categoryId);
 
+    return  sysCategory.fields.slug[locales.default];
+  }
+
+  getBlogPostUrl = async (entry, space, locales, slug) => {
+    const { category } = entry.fields;
+    const categorySlug = this.getCategorySlug(category, space, locales)
+
+    return '/' + categorySlug + '/' + slug;
+  }
+
+  getLandingpageUrl(entry, slug) {
+    return '/' + 'lp' + '/' + slug;
+  }
+
+  getPageUrl= async (entry, space, locales, slug) => {
+    const parentPage = await space.getEntries({
+      'content_type': 'page',
+      select: 'fields.slug',
+      'fields.childPages.sys.id': entry.getSys().id
+    });
+
+    if (parentPage.items.length > 0) {
+      const parentPageSlug = parentPage.items[0].fields.slug[locales.default];
+      return '/' + parentPageSlug + '/' + slug;
+    } else {
+      return '/' + slug;
+    }
+  }
+
+  onLinkButtonClick = async domain => {
+    const { entry, space, locales} = this.props.sdk;
+    const { template, slug } = entry.fields;
+    const slugValue = slug.getValue();
 
     const {
       contentType: {
@@ -48,109 +59,90 @@ export class SidebarExtension extends React.Component {
       }
     } = entry.getSys();
 
-
-    let path;
+    let path = null;
 
     if (contentType === 'blogPost') {
-      const categoryId = category.getValue().sys.id;
-      const sysCategory = await space.getEntry(categoryId);
-      const categorySlug = sysCategory.fields.slug[locales.default];
-      path = '/' + categorySlug + '/';
-    } else if (contentType === 'page') {
-      let parentSlug;
-        if(template.getValue() === "Landingpage") {
-          parentSlug ="lp";
-        } else {
-          const parentPage = await space.getEntries({
-            'content_type': 'page',
-            select: 'fields.slug',
-            'fields.childPages.sys.id': entry.getSys().id
-          });
-          parentSlug = parentPage.items.length > 0 ? parentPage.items[0].fields.slug[locales.default] : null;
-
-        }
-      path = parentSlug ? '/' + parentSlug + '/' : '/';
-
-    } else {
-      path = '/';
+      path = await this.getBlogPostUrl(entry, space, locales, slugValue);
     }
-    return domain + path + (contentSlug && contentSlug.getValue());
-  };
 
-  onPreviewButtonClick = async domain => {
-    const uri = await this.getUrl(domain);
-    window.open(uri);
+    if (contentType === 'page' && template === 'Landingpage') {
+      path = this.getLandingpageUrl(entry, slugValue);
+    }
+
+    if (contentType === 'page'){
+      path = await this.getPageUrl(entry, space, locales, slugValue);
+    }
+
+    window.open(domain + path, '_blank');
   };
 
   onUpdateButtonClick = async () => {
     const {
-      sdk: { entry, space }
+      sdk: { entry, space, notifier }
     } = this.props;
 
     const sysEntry = await space.getEntry(entry.getSys().id);
     await space.updateEntry(sysEntry).then(
       () =>
-        this.props.sdk.notifier.success(
+        notifier.success(
           'Site update requested. This may take a few minutes to process.'
         ),
-      () => this.props.sdk.notifier.error('Site update failed!')
+      () => notifier.error('Site update failed!')
     );
   };
 
   render() {
-    const {
-      parameters: { installation }
-    } = this.props.sdk;
-    const { productionUrl, previewUrl } = installation;
+    const { sdk } = this.props
+    const { productionUrl, previewUrl } = sdk.parameters.installation;
+    const { slug } = sdk.entry.fields;
+    const buttonProps = {
+      buttonType: 'muted',
+      disabled: !slug.getValue(),
+      isFullWidth: true
+    };
+
+    if(!productionUrl || !previewUrl) {
+      return <Paragraph style={{color: "#7f0010"}}>Please configure the extension parameters in contentful</Paragraph>;
+    }
 
     return (
-      <Fragment>
-        <Button
-          buttonType="muted"
-          icon="ExternalLink"
-          isFullWidth={true}
-          testId="open-dialog"
-          onClick={() => this.onPreviewButtonClick(previewUrl)}>
-          Open Preview
-        </Button>
-        <br />
-        <br />
-        <Button
-          buttonType="muted"
-          icon="ExternalLink"
-          isFullWidth={true}
-          testId="open-dialog"
-          onClick={() => this.onPreviewButtonClick(productionUrl)}>
-          Open Live
-        </Button>
-        <br />
-        <br />
-        <Button
-          buttonType="muted"
-          isFullWidth={true}
-          testId="open-dialog"
-          onClick={this.onUpdateButtonClick}>
-          Update Preview
-        </Button>
-      </Fragment>
+        <Grid rowGap="spacingXs">
+          {!slug.getValue() &&
+            <Paragraph style={{color: "#7f0010"}}>
+              Please define a slug to use the preview.
+            </Paragraph>
+          }
+
+          <Button
+            {...buttonProps}
+            testId="open-preview-url"
+            icon = "ExternalLink"
+            onClick={() => this.onLinkButtonClick(previewUrl)}>
+            Open Preview
+          </Button>
+          <Button
+            {...buttonProps}
+            testId="open-production-url"
+            icon = "ExternalLink"
+            onClick={() => this.onLinkButtonClick(productionUrl)}>
+            Open Live
+          </Button>
+          <Button
+            {...buttonProps}
+            testId="update-preview"
+            onClick={this.onUpdateButtonClick}>
+            Update Preview
+          </Button>
+        </Grid>
     );
   }
+
 }
 
 export const initialize = sdk => {
-  if (sdk.location.is(locations.LOCATION_DIALOG)) {
-    ReactDOM.render(<DialogExtension sdk={sdk} />, document.getElementById('root'));
-  } else {
+  if (sdk.location.is(locations.LOCATION_ENTRY_SIDEBAR)) {
     ReactDOM.render(<SidebarExtension sdk={sdk} />, document.getElementById('root'));
   }
 };
 
 init(initialize);
-
-/**
- * By default, iframe of the extension is fully reloaded on every save of a source file.
- * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
- */
-// if (module.hot) {
-//   module.hot.accept();
-// }
